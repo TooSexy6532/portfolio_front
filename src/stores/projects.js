91737985d1216566ffaa97fdefd98a42cf4fe787
Dmitry Papkov <papkovdmitry@gmail.com>
2022-07-14 08:32:53 +0500

    pagination for projects in store

--- src/stores/projects.js
+++ src/stores/projects.js
@@ -6,12 +6,29 @@ export const projectStore = () => {
 	const filter = writable('');
 
 	const filteredProjects = derived([projects, filter], ([$projects, $filter]) => {
-		$projects.filter((p) => p.category!== $filter);
+		if ($filter) return $projects.filter((p) => p.category !== $filter);
+		else return $projects;
 	});
+
+	const currentPage = writable(1);
+
+	const paginatedProjects = derived(
+		[filteredProjects, currentPage],
+		([$projects, $currentPage]) => {
+			const _pagesize = 10;
+
+			const startIndex = $currentPage === 1 ? 0 : ($currentPage - 1) * _pagesize;
+			const endIndex = $currentPage * _pagesize;
+
+			return $projects.slice(startIndex, endIndex);
+		}
+	);
 
 	return {
 		projects,
 		filter,
-		filteredProjects
+		currentPage,
+		filteredProjects,
+		paginatedProjects
 	};
 };
